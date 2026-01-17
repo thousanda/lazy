07af51bcb470ac993050e756be7b5a698a5f47a8
thousanda <k0k1senda@gmail.com>
2026-01-17 16:54:30 +0900

    naive map

--- naive/fp.go
+++ naive/fp.go
@@ -1,14 +1,23 @@
 package naive
 
 // Filter リストの要素を選別する
+// - predicate: ひとつの要素を受け取り、それを残すべきかどうか判断する関数
 // - list: 任意の型のスライス
-// - predicate: listの要素を受け取り、それを残すべきかどうか判断する関数
-func Filter[T any](list []T, predicate func(x T) bool) []T {
+func Filter[T any](predicate func(x T) bool, list []T) []T {
 	result := make([]T, 0, len(list))
 	for _, x := range list {
 		if predicate(x) {
 			result = append(result, x)
 		}
+	}
+	return result
+}
+
+// Map リストのすべての要素に同じ関数を適用する
+func Map[S any, T any](apply func(x S) T, list []S) []T {
+	result := make([]T, 0, len(list))
+	for _, x := range list {
+		result = append(result, apply(x))
 	}
 	return result
 }
--- naive/fp_test.go
+++ naive/fp_test.go
@@ -1,6 +1,7 @@
 package naive
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,14 +10,37 @@ import (
 func TestFilter(t *testing.T) {
 	t.Run("フィルタリングできる", func(t *testing.T) {
 		// Arrange
-		list := []int64{1, 2, 3, 4, 5}
 		isEven := func(x int64) bool {
 			return x%2 == 0
 		}
-
+		list := []int64{1, 2, 3, 4, 5}
 		// Act
-		result := Filter(list, isEven)
-
+		result := Filter(isEven, list)
+		// Assert
 		require.Equal(t, []int64{2, 4}, result)
+	})
+}
+
+func TestMap(t *testing.T) {
+	t.Run("すべての要素に関数を適用できる", func(t *testing.T) {
+		// Arrange
+		double := func(x int64) int64 {
+			return 2 * x
+		}
+		list := []int64{1, 2, 3}
+		// Act
+		result := Map(double, list)
+		// Assert
+		require.Equal(t, []int64{2, 4, 6}, result)
+	})
+	t.Run("入力と出力の型が異なっていてもいい", func(t *testing.T) {
+		// []int -> []string
+		// Arrange
+		toString := strconv.Itoa
+		list := []int{1, 2, 3}
+		// Act
+		result := Map(toString, list)
+		// Assert
+		require.Equal(t, []string{"1", "2", "3"}, result)
 	})
 }
